82d5b1afe05a875a9484bb0f2fcdd3ed2e6a2dd2
Alan-Logrono-S <alanjologrono@hotmail.com>
2026-05-06 17:09:17 -0500

    cliente Distr CAMBIOS

--- src/clases/Cliente.java
+++ src/clases/Cliente.java
@@ -1,14 +1,22 @@
 package clases;
 
-import java.io.IOException;
 import java.rmi.Naming;
 
 public class Cliente {
+
     public static String consultar (int id) throws Exception {
-        String resultado = null;
-        String rmiObjetName = "rmi://localhost/Datos";
-        Servidor servicio = (Servidor)  Naming.lookup(rmiObjetName);
-        resultado = servicio.consultar(id);
+        String resultado=null;
+        String rmiObjectName = "rmi://localhost/Datos";
+        Servidor servicio = (Servidor) Naming.lookup(rmiObjectName);
+        servicio.consultar(id);
         return resultado;
+    }
+
+    public static void main(String[] args) {
+        try {
+            consultar(1);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
